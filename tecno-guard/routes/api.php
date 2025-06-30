28f6d921f74d6379d667800d16226ffeeb9d20ef
RicardoSanchez <rs795384@hotmail.com>
2025-06-30 16:11:26 -0600

    [UPD] Actualizada la ruta user

--- tecno-guard/routes/api.php
+++ tecno-guard/routes/api.php
@@ -15,5 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:api')->prefix('v1')->get('/user', function (Request $request) {
-    return $request->user();
+    return response()->json(["message" => "Información del usuario obtenida correctamente", "data" => $request->user(), "status" => true]);
 });
